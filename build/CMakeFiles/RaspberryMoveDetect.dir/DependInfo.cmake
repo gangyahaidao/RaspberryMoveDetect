ae5cc8b0fce35df21c682dcb555b2a63aac82da4
gangyahaidao <736791342@qq.com>
2018-12-13 09:27:30 +0800

    增加build目录编译测试

---
+++ build/CMakeFiles/RaspberryMoveDetect.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Downloads/RaspberryMoveDetect/src/BGMain_Raspberry.cpp" "/home/pi/Downloads/RaspberryMoveDetect/build/CMakeFiles/RaspberryMoveDetect.dir/src/BGMain_Raspberry.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "../include"
+  "/home/pi/Downloads/bgslibrary/package_bgs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/RaspberryMoveDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RaspberryMoveDetect.dir/src/BGMain_Raspberry.cpp.o"
+  "RaspberryMoveDetect.pdb"
+  "RaspberryMoveDetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RaspberryMoveDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
